12e1e681f2981c8f606ae01a76e812ac8450d67c
Rutwik1801 <65919321+Rutwik1801@users.noreply.github.com>
2020-08-23 14:52:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelector(".js--prev").addEventListener("click",()=>{
+    document.querySelector(".main--1").classList.toggle("opa");
+    console.log("i click");
+});
+document.querySelector(".js--next").addEventListener("click",()=>{
+    document.querySelector(".main--1").classList.toggle("opa");
+    console.log("i click");
+});
